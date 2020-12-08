f55b8e2de76225262b504e4e639cfacc4b67c5ce
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-12-08 14:53:20 -0500

    Add automated QA.

--- src/primo-gateway-pipeline-stack.ts
+++ src/primo-gateway-pipeline-stack.ts
@@ -13,6 +13,7 @@ import { SecretValue } from '@aws-cdk/core'
 import { ArtifactBucket, PipelineNotifications, SlackApproval } from '@ndlib/ndlib-cdk'
 import PrimoGatewayBuildProject from './primo-gateway-build-project'
 import PrimoGatewayBuildRole from './primo-gateway-build-role'
+import PrimoGatewayQaProject from './primo-gateway-qa-project'
 
 const stages = ['test', 'prod']
 
@@ -109,6 +110,18 @@ export default class PrimoGatewayPipelineStack extends cdk.Stack {
       environmentVariables: actionEnvironment,
     })
 
+    // AUTOMATED QA
+    const qaProject = new PrimoGatewayQaProject(this, 'QAProject', {
+      stage: 'test',
+      role: codebuildRole,
+    })
+    const smokeTestsAction = new CodeBuildAction({
+      input: appSourceArtifact,
+      project: qaProject,
+      actionName: 'SmokeTests',
+      runOrder: 98,
+    })
+
     // APPROVAL
     const approvalTopic = new sns.Topic(this, 'PipelineApprovalTopic', {
       displayName: 'PipelineApprovalTopic',
@@ -129,7 +142,7 @@ export default class PrimoGatewayPipelineStack extends cdk.Stack {
     // TEST STAGE
     pipeline.addStage({
       stageName: 'DeployToTest',
-      actions: [deployToTestAction, manualApprovalAction],
+      actions: [deployToTestAction, smokeTestsAction, manualApprovalAction],
     })
 
     // DEPLOY TO PROD
@@ -146,10 +159,22 @@ export default class PrimoGatewayPipelineStack extends cdk.Stack {
       environmentVariables: actionEnvironment,
     })
 
+    // AUTOMATED QA
+    const prodQaProject = new PrimoGatewayQaProject(this, 'QAProjectProd', {
+      stage: 'prod',
+      role: codebuildRole,
+    })
+    const prodSmokeTestsAction = new CodeBuildAction({
+      input: appSourceArtifact,
+      project: prodQaProject,
+      actionName: 'SmokeTests',
+      runOrder: 98,
+    })
+
     // PROD STAGE
     pipeline.addStage({
       stageName: 'DeployToProd',
-      actions: [deployToProdAction],
+      actions: [deployToProdAction, prodSmokeTestsAction],
     })
   }
 }
